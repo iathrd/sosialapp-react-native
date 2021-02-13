51b081ac1d3ebe5cd9b4da053fa36eab90dff9e1
Iqbal Athorid <iqbalathoridd@gmail.com>
2021-02-14 00:33:38 +0800

    created image picker fungsional

--- src/screens/AddPost.js
+++ src/screens/AddPost.js
@@ -1,30 +1,68 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {View, Text, StyleSheet} from 'react-native';
-import {InputField, InputWrapper} from '../styles/AddPost';
+import {
+  InputField,
+  InputWrapper,
+  SubmitBtn,
+  SubmitBtnText,
+  AddImage,
+} from '../styles/AddPost';
 import ActionButton from 'react-native-action-button';
 import Icon from 'react-native-vector-icons/Ionicons';
+import ImagePicker from 'react-native-image-crop-picker';
 
 export default function AddPost() {
+  const [image, setImage] = useState(null);
+
+  const takePhotoFromCamera = () => {
+    ImagePicker.openCamera({
+      width: 1200,
+      height: 780,
+      cropping: true,
+    }).then((image) => {
+      console.log(image);
+      const imageUri = image.path;
+      setImage(imageUri);
+    });
+  };
+
+  const choosePhotoFromLibrary = () => {
+    console.log('PPPPP');
+    ImagePicker.openPicker({
+      width: 1200,
+      height: 780,
+      cropping: true,
+    }).then((image) => {
+      console.log(image);
+      const imageUri = image.path;
+      setImage(imageUri);
+    });
+  };
+
   return (
     <View style={styles.container}>
       <InputWrapper>
+        {image != null ? <AddImage source={{uri: image}} /> : null}
         <InputField
           placeholder="What's on your mind?"
           multiline
           numberOfLines={4}
         />
+        <SubmitBtn onPress={console.log('PPP')}>
+          <SubmitBtnText>Post</SubmitBtnText>
+        </SubmitBtn>
       </InputWrapper>
       <ActionButton buttonColor="rgba(231,76,60,1)">
         <ActionButton.Item
           buttonColor="#9b59b6"
           title="Take Photo"
-          onPress={() => console.log('notes tapped!')}>
+          onPress={takePhotoFromCamera}>
           <Icon name="camera-outline" style={styles.actionButtonIcon} />
         </ActionButton.Item>
         <ActionButton.Item
           buttonColor="#3498db"
           title="Choose Photo"
-          onPress={() => {}}>
+          onPress={choosePhotoFromLibrary}>
           <Icon name="md-images-outline" style={styles.actionButtonIcon} />
         </ActionButton.Item>
       </ActionButton>
